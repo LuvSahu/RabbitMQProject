1a105b1cff3baee258bbbb3ea032d20c0f871e13
Luv sahu <lksahu404@gmail.com>
2022-09-07 21:09:33 +0530

    [Luv SAHU]: RabbitMQ Project Added

---
+++ RabbitMQProject/RabbitMQTicketing/TicketProcessor.MicroService/Consumer/TicketConsumer.cs
@@ -0,0 +1,19 @@
+﻿using MassTransit;
+using Shared.Models.Model;
+using System.Threading.Tasks;
+
+namespace TicketProcessor.MicroService.Consumer
+{
+    public class TicketConsumer : IConsumer<Ticket>
+    {
+        public async Task Consume(ConsumeContext<Ticket> context)
+        {
+            var data = context.Message;
+            //Validate the Ticket Data
+            //Store to Database
+            //Notify the user via Email / SMS
+
+        }
+
+    }
+}
